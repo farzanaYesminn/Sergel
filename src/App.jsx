ada2d512b1256367ca07e425669b1e11f53ac15a
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-09-03 23:19:47 +0600

    nav created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar"
 
 function App() {
  
 
   return (
     <>
+    <Navbar/>
       
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+
+
+  return (
+    <nav>
+        
+    </nav>
+  );
+}
+
+export default Navbar;
