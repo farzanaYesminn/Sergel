9efb5420c52c95f1b922898fe760fed7673f396d
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-09-04 15:48:04 +0600

    navbar design updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 
-
 function Navbar() {
 
 
   return (
     <nav>
-        
+      <div className="container  flex  justify-evenly bg-[#f78da7] bg-opacity-10  mx-auto ">
+        <div className="flex  gap-10">
+          <a href=' ' className="uppercase text-gray-500 font-bold py-5 hover:text-indigo-950"> Professional Site </a>
+          <a href=' ' className="uppercase text-gray-500 font-bold py-5 hover:text-indigo-950">Patient Site </a>
+          
+        </div>
+
+        <div>
+          <img src='src\assets\logo.svg' className="w-[120px] py-5 opacity-90"/>
+        </div>
+
+        <div className="flex  gap-10">
+          <a href=' ' className="uppercase text-gray-500 font-bold py-5 hover:text-indigo-950">Medication </a>
+          <a href=' ' className="uppercase text-gray-500 font-bold py-5 hover:text-indigo-950">prescribing Information </a>
+        </div>
+
+      </div>   
     </nav>
   );
 }
